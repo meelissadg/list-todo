25ae4e3d2484188c51686d174434d657af0317c5
Melissa Diniz <melissa.goncalves@ccc.ufcg.edu.br>
2018-03-23 23:35:10 -0300

    Enviando todoList

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,6 +2,6 @@ class RecipesController < ApplicationController
 
 	def index
 		#recebe as informações que vem do banco de dados - variavel de instancia
-		@recipes = [ 'Tarefa1', 'Tarefa1','Tarefa3' ]
+		@recipes = [ 'Estudar Java', 'Criar Banco de dados','Entregar relatório' ]
  	end
 end
